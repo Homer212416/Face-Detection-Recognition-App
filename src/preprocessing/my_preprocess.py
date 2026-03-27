08a3a90378c4aae5c7e524782474c157ae0f6592
me <me@me>
2026-03-27 13:41:11 -0400

    add/change/delete

--- src/preprocessing/my_preprocess.py
+++ src/preprocessing/my_preprocess.py
@@ -19,37 +19,27 @@ SPLITS_DIR = "data/splits"
 
 
 def step1_crop_all():
-    haar_cascade  = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    raw_folder_ls = os.listdir(RAW_DIR)
 
-    ls_size = len(raw_folder_ls)
-
-    raw_input_path_ls = []
-    processed_output_path_ls = []
+    # load the cascade
+    cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     
-    for i in range(ls_size):
-        raw_input_path = os.path.join(RAW_DIR, raw_folder_ls[i])
-        processed_output_path = os.path.join(PROCESSED_DIR, raw_folder_ls[i])
-        raw_input_path_ls.append(raw_input_path)
-        processed_output_path_ls.append(processed_output_path)
-    
-    
+    os.listdir(RAW_DIR)
         
 
 
 
-def step2_split():
+# def step2_split():
 
-def step3_augment_train():
+# def step3_augment_train():
 
 
 if __name__ == "__main__":
-    for directory in [PROCESS_DIR, SPLITS_DIR]: 
+    for directory in [PROCESSED_DIR, SPLITS_DIR]: 
         if os.path.exists(directory):
             shutil.rmtree(directory) # clear the data if they already exist
         os.makedirs(directory)
     step1_crop_all()
-    step2_split()
-    step3_augment_train()
+    #step2_split()
+    #step3_augment_train()
 
 
